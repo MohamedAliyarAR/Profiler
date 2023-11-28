a13b700ab6d20593299c31e40b4570ce83596eb1
A.R. Mohamed Aliyar <88850090+MohamedAliyarAR@users.noreply.github.com>
2023-11-28 20:52:50 +0530

    beta profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,47 +1,99 @@
-$("#lp").click(function () {
-    window.location.href = "./login.html";
-});
+// const fillIt = (storedData) => {
+//     console.log(storedData);
 
-const email = localStorage.getItem('loggedInEmail');
-
-$('#email').val(email);
-
-if (!email) {
-    window.location.href = './index.html'
-}
-
+// }
 $(document).ready(function () {
-    $(document).on('submit', '#profileForm', postIt);
+    //     const fillFormWithUserData = () => {
+    //         const storedData = { email: localStorage.getItem('email') }
+    //         $.ajax({
+    //             type: 'POST',
+    //             url: 'php/profile.php',
+    //             data: storedData,
+    //             dataType: 'json',
+    //             success: function (response) {
+    //                 // if (response.success) {
+    //                 console.log(response.message)
+    //                 // }
+    //             },
+    //             error: function (error) {
+    //                 console.error('Error updating profile:', error);
+    //             }
+    //         });
 
-});
+    //     }
 
-$('#signout').click(function () {
-    localStorage.removeItem('loggedInEmail')
-    window.location.href = './index.html'
-})
+    // fillFormWithUserData();
 
-const postIt = (e) => {
-    e.preventDefault();
-    $.ajax({
+    const email = localStorage.getItem('loggedInEmail');
 
-        type: 'POST',
-        url: 'php/profile.php', 
-        data: {
+    $('#email').val(email);
+
+
+
+    const postIt = (e) => {
+        e.preventDefault();
+
+        const formData = {
             'name': $('#name').val(),
             'age': $('#age').val(),
             'dob': $('#dob').val(),
             'phonenumber': $('#phonenumber').val(),
             'email': $('#email').val()
-        },
-        dataType: 'json',
-        success: function (response) {
-            // Handle success response (e.g., display a success message)
-            alert(response.message);
-        },
-        error: function (error) {
-            // Handle error response (e.g., display an error message)
-            console.error('Error updating profile:', error);
-        }
+            // }
+        };
+
+
+        $.ajax({
+
+            type: 'POST',
+            url: 'php/profile.php',
+            data: formData,
+            dataType: 'json',
+            success: function (response) {
+
+                alert(response.message);
+            },
+            error: function (error) {
+
+                console.error('Error updating profile:', error);
+            }
+        });
+    }
+
+    $('.edit-icon').on('click', function () {
+
+        var inputFieldId = $(this).data('input');
+
+        var value = $(inputFieldId).val();
+
+        var fieldName = $(inputFieldId).attr('name');
+
+        var data = {
+            fieldName: fieldName,
+            value: value,
+            'email': email
+        };
+        console.log(data);
+        $.ajax({
+            type: 'POST',
+            url: 'php/profile.php',
+            data: data,
+            dataType: 'json',
+            success: function (response) {
+
+                alert(response.message);
+            },
+            error: function (error) {
+
+                console.error(error);
+            }
+        });
     });
 
-}
+
+    $('#signout').click(function () {
+        localStorage.removeItem('loggedInEmail')
+        window.location.href = './index.html'
+    })
+
+})
