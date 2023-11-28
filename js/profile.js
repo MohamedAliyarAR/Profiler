ceab06071c846a8071c9fdb63f55adadbcbba094
A.R. Mohamed Aliyar <88850090+MohamedAliyarAR@users.noreply.github.com>
2023-11-28 21:19:58 +0530

    beta 1 profile.js

--- js/profile.js
+++ js/profile.js
@@ -3,7 +3,83 @@
 
 // }
 $(document).ready(function () {
-    //     const fillFormWithUserData = () => {
+    //     const fillFormWithUserData = ()
+$(document).ready(function () {
+
+
+    const email = localStorage.getItem('loggedInEmail');
+
+    $('#email').val(email);
+
+
+    $(document).on('submit', '#profileForm',
+        (e) => {
+            e.preventDefault();
+
+            const formData = {
+                'name': $('#name').val(),
+                'age': $('#age').val(),
+                'dob': $('#dob').val(),
+                'phonenumber': $('#phonenumber').val(),
+                'email': $('#email').val()
+                // }
+            };
+
+
+            $.ajax({
+
+                type: 'POST',
+                url: 'php/profile.php',
+                data: formData,
+                dataType: 'json',
+                success: function (response) {
+
+                    alert(response.message);
+                },
+                error: function (error) {
+
+                    console.error('Error updating profile:', error);
+                }
+            });
+        })
+
+    $('.edit-icon').on('click', function () {
+
+        var inputFieldId = $(this).data('input');
+
+        var value = $(inputFieldId).val();
+
+        var fieldName = $(inputFieldId).attr('name');
+
+        var data = {
+            fieldName: fieldName,
+            value: value,
+            'email': email
+        };
+        console.log(data);
+        $.ajax({
+            type: 'POST',
+            url: 'php/profile.php',
+            data: data,
+            dataType: 'json',
+            success: function (response) {
+
+                alert(response.message);
+            },
+            error: function (error) {
+
+                console.error(error);
+            }
+        });
+    });
+
+
+    $('#signout').click(function () {
+        localStorage.removeItem('loggedInEmail')
+        window.location.href = './index.html'
+    })
+
+}) => {
     //         const storedData = { email: localStorage.getItem('email') }
     //         $.ajax({
     //             type: 'POST',
