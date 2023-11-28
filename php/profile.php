c1b2447bb9b6e3bf5a611d0758a27d5409ff139e
A.R. Mohamed Aliyar <88850090+MohamedAliyarAR@users.noreply.github.com>
2023-11-28 20:50:51 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -35,7 +35,81 @@ $customerDetails = $profile->customers;
     ];
     
     
-    
+    <?php
+require '../vendor/autoload.php';
+
+use MongoDB\Client;
+
+$client = new Client("mongodb://localhost:27017");
+
+$profile = $client->profile;
+$customerDetails = $profile->customers;
+
+$response = ['success' => false, 'message' => 'Invalid request'];
+
+    if (!empty($_POST['email'])) {
+        $email = $_POST['email'];
+
+        if (!empty($_POST['fieldName']) && !empty($_POST['value'])) {
+            $fieldName = $_POST['fieldName'];
+            $value = $_POST['value'];
+            
+
+            $updateArray = [
+                $fieldName => $value
+            ];
+
+            // Update the data in MongoDB for the specific email
+            $filter = ['email' => $email];
+            $update = [
+                '$set' => $updateArray,
+            ];
+
+            $result = $customerDetails->updateOne($filter, $update, ['$upsert' => true]);
+
+            if ($result->getModifiedCount() > 0 || $result->getUpsertedCount() > 0) {
+                $response = ['success' => true, 'message' => 'Profile updated successfully'];
+            } else {
+                $response['message'] = 'Error updating profile';
+            }
+        } 
+        else {
+           
+            $name = $_POST['name'];
+            $age = $_POST['age'];
+            $dob = $_POST['dob'];
+            $phonenumber = $_POST['phonenumber'];
+
+           
+            $filter = ['email' => $email];
+            $update = [
+                '$set' => [
+                    'name' => $name,
+                    'age' => $age,
+                    'dob' => $dob,
+                    'phonenumber' => $phonenumber,
+                ],
+            ];
+
+            $result = $customerDetails->updateOne($filter, $update, ['upsert' => true]);
+
+            if ($result->getModifiedCount() > 0 || $result->getUpsertedCount() > 0) {
+                $response = ['success' => true, 'message' => 'Profile updated successfully'];
+            } else {
+                $response['message'] = 'Error updating profile';
+            }
+        }
+    } else {
+        $response['message'] = 'Missing required fields';
+    }
+
+
+header('Content-Type: application/json');
+
+
+echo json_encode($response);
+?>
+
 
     
     $result = $customerDetails->updateOne($filter, $update, ['upsert' => true]);
