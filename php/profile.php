f1aed9c13b2ec73ae388340366d67f08e1197ab5
A.R. Mohamed Aliyar <88850090+MohamedAliyarAR@users.noreply.github.com>
2023-11-28 20:51:55 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -1,41 +1,4 @@
 <?php
-
-require '../vendor/autoload.php';
-
-use MongoDB\Client;
-
-// Connect to MongoDB
-$client = new Client("mongodb://localhost:27017");
-
-// Select the "profile" database and "customers" collection
-$profile = $client->profile;
-$customerDetails = $profile->customers;
-
-
-
-    
-    if(!empty($_POST['name']) && !empty($_POST['age']) && !empty($_POST['dob']) && !empty($_POST['phonenumber']) && !empty($_POST['email'])){
-    $name = $_POST['name'];
-    $age = $_POST['age'];
-    $dob = $_POST['dob'];
-    $phonenumber = $_POST['phonenumber'];
-    $email = $_POST['email'];
-
-    
-
-    // Update the data in MongoDB for the specific email
-    $filter = ['email' => $email];
-    $update = [
-        '$set' => [
-            'name' => $name,
-            'age' => $age,
-            'dob' => $dob,
-            'phonenumber' => $phonenumber,
-        ],
-    ];
-    
-    
-    <?php
 require '../vendor/autoload.php';
 
 use MongoDB\Client;
@@ -108,21 +71,4 @@ header('Content-Type: application/json');
 
 
 echo json_encode($response);
-?>
-
-
-    
-    $result = $customerDetails->updateOne($filter, $update, ['upsert' => true]);
-
-    
-    if ($result->getModifiedCount() > 0 || $result->getUpsertedCount() > 0) {
-        $response = ['success' => true, 'message' => 'Profile updated successfully'];
-    } else {
-        $response = ['success' => false, 'message' => 'Error updating profile'];
-    }
-
-    
-    header('Content-Type: application/json');
-    echo json_encode($response);
-    }
 ?>
