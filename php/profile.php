b433e6ddb314a0bdfcebe8096bd6169bf32ee680
A.R. Mohamed Aliyar <88850090+MohamedAliyarAR@users.noreply.github.com>
2023-11-29 23:30:01 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -3,46 +3,59 @@ require '../vendor/autoload.php';
 
 use MongoDB\Client;
 
-$client = new Client("mongodb://localhost:27017");
+
+$mongoHost = "monorail.proxy.rlwy.net";
+$mongoPassword = "ba3dFdf5CcEFdA62aBf6gFe2eeEGEdcC";
+$mongoPort = 34977;
+$mongoUser = "mongo";
+
+$connectionString = "mongodb://$mongoUser:$mongoPassword@$mongoHost:$mongoPort";
+
+$client = new Client($connectionString);
+
+
 
 $profile = $client->profile;
 $customerDetails = $profile->customers;
 
 $response = ['success' => false, 'message' => 'Invalid request'];
 
-    if (!empty($_POST['email'])) {
-        $email = $_POST['email'];
+    
+        
 
-        if (!empty($_POST['fieldName']) && !empty($_POST['value'])) {
-            $fieldName = $_POST['fieldName'];
-            $value = $_POST['value'];
-            
+if (!empty($_POST['fieldName']) && !empty($_POST['value']) && !empty($_POST['email'])) {
+    $fieldName = $_POST['fieldName'];
+    $value = $_POST['value'];
+    $email = $_POST['email'];
 
-            $updateArray = [
-                $fieldName => $value
-            ];
+    $updateArray = [
+        $fieldName => $value
+    ];
 
-            // Update the data in MongoDB for the specific email
-            $filter = ['email' => $email];
-            $update = [
-                '$set' => $updateArray,
-            ];
+    // Update the data in MongoDB for the specific email
+    $filter = ['email' => $email];
+    $update = [
+        '$set' => $updateArray,
+    ];
 
-            $result = $customerDetails->updateOne($filter, $update, ['$upsert' => true]);
+    try {
+        $result = $customerDetails->updateOne($filter, $update, ['upsert' => true]);
 
-            if ($result->getModifiedCount() > 0 || $result->getUpsertedCount() > 0) {
-                $response = ['success' => true, 'message' => 'Profile updated successfully'];
-            } else {
-                $response['message'] = 'Error updating profile';
-            }
-        } 
-        else {
-           
+        if ($result->getModifiedCount() > 0 || $result->getUpsertedCount() > 0) {
+            $response = ['success' => true, 'message' => 'Profile updated successfully'];
+        } else {
+            $response['message'] = 'Error updating profile';
+        }
+    } catch (Exception $e) {
+        $response['message'] = 'MongoDB error: ' . $e->getMessage();
+    }
+} elseif (!empty($_POST['email']) && !empty($_POST['name']) && !empty($_POST['age']) && !empty($_POST['dob']) && !empty($_POST['phonenumber'])) {
+
             $name = $_POST['name'];
             $age = $_POST['age'];
             $dob = $_POST['dob'];
             $phonenumber = $_POST['phonenumber'];
-
+            $email = $_POST['email'];
            
             $filter = ['email' => $email];
             $update = [
@@ -58,11 +71,14 @@ $response = ['success' => false, 'message' => 'Invalid request'];
 
             if ($result->getModifiedCount() > 0 || $result->getUpsertedCount() > 0) {
                 $response = ['success' => true, 'message' => 'Profile updated successfully'];
-            } else {
-                $response['message'] = 'Error updating profile';
+            } 
+             else {
+                $response['message'] = 'Error updating profile!!';
             }
         }
-    } else {
+    
+    
+    else {
         $response['message'] = 'Missing required fields';
     }
 
