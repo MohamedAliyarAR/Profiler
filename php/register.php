bd2e5ddc9d7e97ab28fb1b1dec762b447f4d308d
A.R. Mohamed Aliyar <88850090+MohamedAliyarAR@users.noreply.github.com>
2023-11-29 23:30:26 +0530

    Update register.php

--- php/register.php
+++ php/register.php
@@ -1,87 +1,39 @@
 <?php
 
-require '../vendor/autoload.php';
-
-$client= new MongoDB\Client;
-        $profile= $client->profile;
-        $customerDetails = $profile->customers;
-
-
-$userdata = $_POST;
-
-
-
-
-        // zqlll
-$hostname       = "localhost";
-$username       = "root";
-$dbpassword     = "";  
-$databasename   = "profileAuth";
-
-// Create connection SQL
-
-$conn = mysqli_connect($hostname, $username, $dbpassword, $databasename, 3307);
-
-// Check connection SQL
-if (!$conn) {
-    die("Unable to Connect database: " . mysqli_connect_error());
+$hostname     = "viaduct.proxy.rlwy.net";
+$username     = "root";
+$dbpassword   = "66ghc5DEc1dFg-5HFa6AbG1h5C1Cdc5e"; 
+$databasename = "railway";
+$port         = 48357;
+$conn = new mysqli($hostname, $username, $dbpassword, $databasename, $port);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-reg($conn,$userdata);
-// soldiers ATTACK
-function reg($conn,$userdata) {
-    $email  = $userdata['email'];
-    $password       = $userdata['password'];
-
-if(!empty($email) && !empty($password)){
-    
 
 
-$select = "SELECT email FROM customertable WHERE email = ? LIMIT 1";
-$query = "INSERT INTO customertable (email, password) VALUES (?, ?)";
+if(!empty($_POST['email']) && !empty($_POST['password'])){
+$email    = $_POST['email'];
+$password = $_POST['password'];
 
-$stmt = $conn->prepare($select);
-$stmt->bind_param("s", $email);  
+$selectQuery = "SELECT email FROM customertable WHERE email = ? LIMIT 1";
+$stmt = $conn->prepare($selectQuery);
+$stmt->bind_param("s", $email);
 $stmt->execute();
 $stmt->store_result();
 $rnum = $stmt->num_rows;
 
-
-
 if ($rnum == 0) {
-    $stmt->close();
-
-    $stmt = $conn->prepare($query);
-    
-    // Bind parameters
-    $stmt->bind_param("ss", $email, $password);  // Corrected variable name
-
-    $execute = $stmt->execute();
-
-    
-        
-        
-        $successMessage = array("success" => "User registration successful");
-        echo json_encode($successMessage);
-        
-       
-
-     
-} 
-else {
-    $errorMessage = array("error" => "Already registered");
-    echo json_encode($errorMessage);
+    $insertQuery = "INSERT INTO customertable (email, password) VALUES (?, ?)";
+    $stmt = $conn->prepare($insertQuery);
+    $stmt->bind_param("ss", $email, $password);
+    $stmt->execute();
+    header('Content-Type: application/json');
+echo json_encode(['success' => 'Account created successfully.']);
+} else {
+    header('Content-Type: application/json');
+echo json_encode(['error' => 'Email already exists']);
 }
-
-
-
-
 $stmt->close();
 $conn->close();
-
-
-
-
 }
-}
-
-?> 
+?>
